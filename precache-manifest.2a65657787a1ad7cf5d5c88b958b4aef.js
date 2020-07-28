326ec0509d1726d6afba7f1db931a27748d7f544
Bernardo Garza <bernardo.g.landa@gmail.com>
2020-07-28 07:03:28 -0500

    Updates

---
+++ precache-manifest.2a65657787a1ad7cf5d5c88b958b4aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bef335643801802d9314c678cd1649",
+    "url": "/three.js-practice-project/index.html"
+  },
+  {
+    "revision": "6d4460be205e7912f262",
+    "url": "/three.js-practice-project/static/css/main.9b890a60.chunk.css"
+  },
+  {
+    "revision": "c9d3d386eca20dae1b31",
+    "url": "/three.js-practice-project/static/js/2.632d2302.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/three.js-practice-project/static/js/2.632d2302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4460be205e7912f262",
+    "url": "/three.js-practice-project/static/js/main.4b89fde3.chunk.js"
+  },
+  {
+    "revision": "4b07f0f3f961776ae727",
+    "url": "/three.js-practice-project/static/js/runtime-main.1c2ca9c0.js"
+  }
+]);
